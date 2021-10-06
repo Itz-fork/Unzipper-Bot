99ba8188597000ff48ca15d12d79f9de783ad4e6
Vampire421 <91833744+Vampire421@users.noreply.github.com>
2021-10-07 02:06:27 +0530

    Update bot_data.py

--- unzipper/modules/bot_data.py
+++ unzipper/modules/bot_data.py
@@ -117,7 +117,7 @@ Hi **{}**, I'm **HF Projects Unzipper Bot** 😇!
  - `Please make sure that you selected the right mode!`
  - `May be Your archive format isn't supported 😔`
 
-**Please report this at @HFBots if you think this is a serious error**
+**Please report this at @HFBotsupdates if you think this is a serious error**
     """
 
     ERROR_TXT = """
@@ -126,7 +126,7 @@ Hi **{}**, I'm **HF Projects Unzipper Bot** 😇!
 **ERROR:** {}
 
 
-**Please report this at @HFbots if you think this is a serious error**
+**Please report this at @HFbotsupdates if you think this is a serious error**
     """
 
     CANCELLED_TXT = """
