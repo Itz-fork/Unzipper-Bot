7dc05d745eab8234d0199356dc7f30a6873a2527
Itz-fork <git.itzfork@gmail.com>
2022-07-07 16:51:22 +0530

    Updated copyright header

--- unzipper/modules/settings.py
+++ unzipper/modules/settings.py
@@ -1,5 +1,14 @@
-# Copyright (c) 2022 Itz-fork
-# Don't kang this else your dad is gae
+# ===================================================================== #
+#                      Copyright (c) 2022 Itz-fork                      #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  #
+# See the GNU General Public License for more details.                  #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program. If not, see <http://www.gnu.org/licenses/>   #
+# ===================================================================== #
 
 from pyrogram import filters
 from unzipper import unzipperbot
