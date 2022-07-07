77a1453ec7d8979874cce0fb2fb528cf7a035600
Itz-fork <git.itzfork@gmail.com>
2022-07-07 17:44:28 +0530

    Test

--- unzipper/__main__.py
+++ unzipper/__main__.py
@@ -10,16 +10,16 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>   #
 # ===================================================================== #
 
-from config import Config
-from pyrogram import idle
 from os import makedirs, path
-from unzipper import unzip_client
+from config import Config
 from .helpers_nexa.checks import check_log_channel
+from pyrogram import idle
 
 
 if __name__ == "__main__":
     if not path.isdir(Config.DOWNLOAD_LOCATION):
         makedirs(Config.DOWNLOAD_LOCATION)
+    from . import unzip_client
     unzip_client.start()
     print("Checking Log Channel ...")
     check_log_channel()
