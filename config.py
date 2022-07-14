f81ef374754ef77f1d14b8b003145fd5c3fbd348
Itz-fork <git.itzfork@gmail.com>
2022-07-14 14:58:31 +0530

    Fixed MAX_DOWNLOAD_SIZE config

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     MONGODB_URL = os.environ.get("MONGODB_URL")
     GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN")
     # Optional
-    MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) or 5368709120
+    MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
     # Constents
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/NexaBots"
     TG_MAX_SIZE = 2040108421
